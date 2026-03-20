84edafa4d24c3eebba183cb2179a5cf6e50d046c
unknown <aherakash785@gmail.com>
2026-03-20 23:38:18 +0530

    small change

--- app.js
+++ app.js
@@ -77,15 +77,6 @@ app.listen(8080, () => {
 console.log("sever is listening");
 });
 
-app.use("*",(req, res, next) => {
-  next(new ExpressError(404, "Page Not Found!"));
-});
 
-
-
-app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).send(message);
-});
 
 
